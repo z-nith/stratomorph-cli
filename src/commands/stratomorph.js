a8579928960789e72aabe825d4273e2fb118dde8
Jmeza081 <jmeza081@gmail.com>
2018-12-23 20:18:43 -0600

    update example using ora

--- src/commands/stratomorph.js
+++ src/commands/stratomorph.js
@@ -1,9 +1,20 @@
-
 module.exports = {
   name: 'stratomorph',
   run: async toolbox => {
-    const { print, colorExtractor } = toolbox
-    colorExtractor();
+    const { print, colorExtractor, imageReader } = toolbox
+    const ora = require('ora')
 
+    const spinner = ora('Reading Image').start()
+    let imageCanvas = await imageReader('./sample_images/sample.jpg')
+    spinner.succeed('Image read')
+
+
+
+    spinner.text = '';
+    colorExtractor(imageCanvas);
+    spinner.succeed('Image Analyzed');
+
+
+    print.info('Done')
   }
 }
