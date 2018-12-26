ae347b1f4bd5f458e0c90a4233b6d4fad33caf8d
Jmeza081 <jmeza081@gmail.com>
2018-12-25 19:30:15 -0600

    implement beginning steps

--- src/commands/stratomorph.js
+++ src/commands/stratomorph.js
@@ -1,20 +1,20 @@
 module.exports = {
-  name: 'stratomorph',
-  run: async toolbox => {
-    const { print, colorExtractor, imageReader } = toolbox
-    const ora = require('ora')
+    name: 'stratomorph',
+    run: async (toolbox) => {
+        let dummyPath = './sample_images/sample.jpg';
+        const { print, colorExtractor, imagePrimer, paletteReporter } = toolbox;
+        const chalk = require('chalk');
 
-    const spinner = ora('Reading Image').start()
-    let imageCanvas = await imageReader('./sample_images/sample.jpg')
-    spinner.succeed('Image read')
+        const spinner = print.spin('Loading Image');
+        let imageBuffer = await imagePrimer.loadImage(dummyPath);
+        spinner.succeed('Image Loaded. Building Image Canvas...');
+        let imageCanvas = await imagePrimer.buildCanvas(imageBuffer);
+        spinner.succeed('Image Canvas Built. Analyzing Image...');
 
+        let colorPalette = await colorExtractor(imageCanvas);
+        spinner.succeed('🌄 Analysis Complete. Building Color Palette...');
 
-
-    spinner.text = '';
-    colorExtractor(imageCanvas);
-    spinner.succeed('Image Analyzed');
-
-
-    print.info('Done')
-  }
-}
+        spinner.stopAndPersist({ symbol: '🎨', text: 'Color Palette Ready!' });
+        paletteReporter.printColorTable(colorPalette);
+    },
+};
