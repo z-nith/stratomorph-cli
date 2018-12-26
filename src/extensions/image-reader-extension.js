6766fdcd45b8b74cd653b833aba9b5cb2bafe950
Jmeza081 <jmeza081@gmail.com>
2018-12-25 19:31:20 -0600

    use image prop as export

--- src/extensions/image-reader-extension.js
+++ src/extensions/image-reader-extension.js
@@ -1,18 +1,10 @@
-module.exports = toolbox => {
-  toolbox.imageReader = async imagePath => {
-    const Canvas = require('canvas')
-    const Image = Canvas.Image
+// Import functionality from our user-defined tool box
+const loadImage = require('../toolbox/image_prep/image-reader');
+const buildCanvas = require('../toolbox/image_prep/canvas-builder');
 
-    let img
-    let can
-    let ctx
-
-    img = new Image()
-    img.src = toolbox.filesystem.read(imagePath, 'buffer')
-    can = Canvas.createCanvas(img.width, img.height)
-    ctx = can.getContext('2d')
-    ctx.drawImage(img, 0, 0, img.width, img.height)
-
-    return can;
-  }
-}
+module.exports = (toolbox) => {
+    toolbox.imagePrimer = {
+        loadImage,
+        buildCanvas,
+    };
+};
