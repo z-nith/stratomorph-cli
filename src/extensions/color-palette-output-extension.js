426bf944068d24398d455e8afe40a9ded1db9138
Jmeza081 <jmeza081@gmail.com>
2018-12-27 17:54:43 -0600

    remove in favor of different naming convention

--- src/extensions/color-palette-output-extension.js
+++
@@ -1,8 +0,0 @@
-// Import functionality from our user-defined tool box
-const printColorTable = require('../toolbox/color_palette_output/table-generator');
-
-module.exports = (toolbox) => {
-    toolbox.paletteReporter = {
-        printColorTable,
-    };
-};
