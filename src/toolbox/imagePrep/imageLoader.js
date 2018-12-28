36ac8a663f6a7eabbec76e8cde365697141c144f
Jmeza081 <jmeza081@gmail.com>
2018-12-28 16:38:59 -0600

    add some TODOs

--- src/toolbox/imagePrep/imageLoader.js
+++ src/toolbox/imagePrep/imageLoader.js
@@ -1,4 +1,9 @@
 const { filesystem } = require('gluegun/filesystem');
+/**
+ * TODO:
+ * 1. Need to add a function that can handle dynamically loading an image from a filesystem or a url
+ * 2. Need to add graceful error handling when system fails (research needed on proper way to do this.)
+ */
 
 /**
  * Reads an image from the local file system as a buffer
@@ -10,6 +15,7 @@ const loadImage = (imagePath) => {
     const Canvas = require('canvas');
     const Image = Canvas.Image;
     let img = new Image();
+
     img.src = filesystem.read(imagePath, 'buffer');
 
     return img;
