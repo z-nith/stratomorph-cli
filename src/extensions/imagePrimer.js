f8370d44a9719ae7f3c6b26d9ad4ea7297dbe59f
Jmeza081 <jmeza081@gmail.com>
2018-12-29 18:24:32 -0600

    move all tasks here

--- src/extensions/imagePrimer.js
+++ src/extensions/imagePrimer.js
@@ -7,10 +7,55 @@
  * 2. Using Loaded image, build a Canvas instance that can digested by
  *    Image Quantifier algorithm.
  */
-const loadImage = require('../toolbox/imagePrep/imageLoader');
-const buildCanvas = require('../toolbox/imagePrep/canvasBuilder');
+const { filesystem } = require('gluegun/filesystem');
+const { ImgSourceNotFoundError } = require('../lib/errorClasses');
 
-module.exports = (toolbox) => {
+/**
+ * Builds a Canvas object from the loaded image buffer
+ * @param {ImageBuffer} image
+ *
+ * returns canvas object
+ */
+const buildCanvas = (image) => {
+    const Canvas = require('canvas');
+
+    let canvas = Canvas.createCanvas(image.width, image.height);
+    let context = canvas.getContext('2d');
+    context.drawImage(image, 0, 0, image.width, image.height);
+
+    return canvas;
+};
+
+/**
+ * TODO:
+ * 1. Need to add a function that can handle dynamically loading an image from a filesystem or a url
+ * 2. Need to add graceful error handling when system fails (research needed on proper way to do this.)
+ */
+
+/**
+ * Reads an image from the local file system as a buffer
+ * @param {string} imagePath
+ *
+ * returns image buffer.
+ */
+const loadImage = (imagePath) => {
+    const Canvas = require('canvas');
+    const Image = Canvas.Image;
+    let img = new Image();
+
+    let imageBuffer = filesystem.read(imagePath, 'buffer');
+    if (!imageBuffer) {
+        throw new ImgSourceNotFoundError(
+            `Unable Find Image Resource at ${imagePath}, please verify your input path`,
+        );
+    }
+
+    img.src = imageBuffer;
+
+    return img;
+};
+
+module.exports = async (toolbox) => {
     toolbox.imagePrimer = {
         loadImage,
         buildCanvas,
