082fb6d78ed9b5654048890a554af1cdf56dc2bd
Jmeza081 <jmeza081@gmail.com>
2018-12-30 21:11:11 -0600

    add error classes for network images

--- src/lib/errorClasses.js
+++ src/lib/errorClasses.js
@@ -1,12 +1,29 @@
 class ImgSourceNotFoundError extends Error {
     constructor(...args) {
-        super(...args)
-        Error.captureStackTrace(this, ImgSourceNotFoundError)
+        super(...args);
+        Error.captureStackTrace(this, ImgSourceNotFoundError);
     }
 }
+
+class networkImageFailedToLoadError extends Error {
+    constructor(...args) {
+        super(...args);
+        Error.captureStackTrace(this, ImgSourceNotFoundError);
+    }
+}
+
+class FailedToBuildImageCanvas extends Error {
+    constructor(...args) {
+        super(...args);
+        Error.captureStackTrace(this, FailedToBuildImageCanvas);
+    }
+}
+
 class InvalidArgumentError extends Error {}
 
 module.exports = {
     ImgSourceNotFoundError,
     InvalidArgumentError,
+    FailedToBuildImageCanvas,
+    networkImageFailedToLoadError
 };
