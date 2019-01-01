46475a9755fd18eb47b1c9aa8f255fd94dd4ce0b
Jmeza081 <jmeza081@gmail.com>
2018-12-31 20:48:39 -0600

    add ignore files

--- src/commands/htmlify.js
+++ src/commands/htmlify.js
@@ -3,20 +3,26 @@
  * 1. Need create a template html file for this command to be filled up by palette
  * 2. Handle additional options for tablifying, and output files based on colors
  */
-var preCommandCheck = require('../lib/argParser');
-const { USAGE_STRINGS } = require('../constants');
+var { aggregateIgnoredOptions, hasActionableArguments } = require('../lib/argValidator');
+const { USAGE_STRINGS } = require('../lib/constants');
 
 module.exports = {
     name: 'htmlify',
     run: async (toolbox) => {
-        const { print, colorQuantifier, imagePrimer, paletteReporter } = toolbox;
+        const { print, colorQuantization, imagePrimer, paletteReporter } = toolbox;
 
         // Step 1: Gather params and options from CLI input.
         let params = toolbox.parameters.string;
         let options = toolbox.parameters.options;
 
+        console.log(options);
+
         // Only continue with execution if proper input was supplied
-        if (preCommandCheck(params)) {
+        if (hasActionableArguments(params)) {
+            const ignoredOptions = aggregateIgnoredOptions(options);
+            print.warning(
+                `The following options could not be interpreted and will be ingored: ${ignoredOptions}`,
+            );
             var spinner;
 
             try {
@@ -30,7 +36,7 @@ module.exports = {
                 spinner.succeed('Image Canvas Built. Analyzing Image...');
 
                 // Build color palette by analyzing image colors
-                let colorPalette = await colorQuantifier(imageCanvas);
+                let colorPalette = await colorQuantization(imageCanvas);
                 spinner.succeed('🌄 Analysis Complete. Building Color Palette...');
 
                 // Output resulting palette based on options
